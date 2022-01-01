88cee47dcda4ab80a65f82390ea04eea532492a1
Ben Lahav <benlahav1990@gmail.com>
2022-01-01 11:57:46 +0200

    added data preproccesing to remove Nan values and remove noise cols

--- app/data_preprocessing.py
+++ app/data_preprocessing.py
@@ -3,14 +3,14 @@ from pathlib import Path
 import pandas as pd
 
 
-
 def remove_noise_cols(df: pd.DataFrame) -> pd.DataFrame:
     valid_cols = ["track_uri", "track_name", "artist_name", "artist_popularity", "artist_followers",
-                  "artist_genres","album", "track_popularity", "track_danceability", "track_energy",
+                  "artist_genres", "album", "track_popularity", "track_danceability", "track_energy",
                   "track_loudness", "track_tempo", "track_duration_ms", "is_explict_content"]
     cols_to_remove = list(filter(lambda x: x not in valid_cols, df.columns))
 
     return df.drop(columns=cols_to_remove)
+
 
 def remove_nan_values(df: pd.DataFrame) -> pd.DataFrame:
     return df.dropna()
