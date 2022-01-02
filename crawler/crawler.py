b30f2575b2b2ea3e6d96225caffada4bd611f491
Ben Bukai <bukai7ben@gmail.com>
2022-01-02 21:46:41 +0200

    refactor crawler of www.famousbirthdays.com

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -2,16 +2,18 @@ import pandas as pd
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
+from pathlib import Path
 import time
 
-i = 0
+found_counter = 1
+not_found_counter = 1
 not_found_artists = []
-df = pd.read_csv("tracks.csv", sep=",")
-artists = df["artist_name"].unique()
-pd.DataFrame(artists)
+df = pd.read_csv(Path.cwd().parent / "data" / "tracks.csv", sep=",")
+artists = df["artist_name"].unique().tolist()
+
 driver = webdriver.Chrome("chromedriver.exe")
 driver.get("https://www.famousbirthdays.com/")
-artists_df = pd.DataFrame(columns=["artist", "age", "birth place"])
+artists_df = pd.DataFrame(columns=["artist", "age", "origin"])
 
 for artist in artists:
 
@@ -21,23 +23,42 @@ for artist in artists:
 
     try:
         age = driver.find_element(By.XPATH, "//a[contains(text(),'years old')]").text
-        birth_place = driver.find_element(By.XPATH, "//h6[contains(text(),'Birthplace')]/parent::div").text
+        origin = driver.find_element(By.XPATH, "//h6[contains(text(),'Birthplace')]/parent::div").text
+        origin = origin.split("\n")[1]
+        age = age.split(" ")[0]
+        row = [artist, age, origin]
+        artists_df.loc[found_counter] = row
+        print(f"{found_counter}) {artist}, {age}, {origin}\n")
+        found_counter += 1
     except NoSuchElementException:
-        not_found_artists.append(artist)
-        print(f"{artist} not found")
-        continue
+        try:
+            origin = driver.find_element(By.XPATH, "//h6[contains(text(),'Origin')]/parent::div").text
+            age = ""
+            origin = origin.split("\n")[1]
+            row = [artist, age, origin]
+            artists_df.loc[found_counter] = row
+            print(f"{found_counter}) {artist}, {age}, {origin}\n")
+            found_counter += 1
+        except NoSuchElementException:
+            try:
+                age = driver.find_element(By.XPATH, "//span[contains(text(),'age')]/parent::a").text
+                age = age.split("age ")[1]
+                origin = driver.find_element(By.XPATH, "//h6[contains(text(),'Birthplace')]/parent::div").text
+                origin = origin.split("\n")[1]
+                row = [artist, age, origin]
+                artists_df.loc[found_counter] = row
+                print(f"{found_counter}) {artist}, {age}, {origin}\n")
+                found_counter += 1
+            except NoSuchElementException:
+                not_found_artists.append(artist)
+                print(f"not found {not_found_counter} artists\n")
+                not_found_counter += 1
+                continue
 
 
-    birth_place = birth_place.split("\n")[1]
-    age = age.split(" ")[0]
-    row = [artist, age, birth_place]
-    artists_df.loc[i] = row
-    print(f"{i}: {artist}")
-    i += 1
-
-artists_df.to_csv("artists.csv", index=False)
+artists_df.to_csv(Path.cwd().parent / "data" / "artists_origin_and_age.csv")
 not_found_artists_pd = pd.DataFrame(not_found_artists)
-not_found_artists_pd.to_csv("not_found.csv", index=False)
+not_found_artists_pd.to_csv(Path.cwd().parent / "data" / "not_found.csv")
 
 driver.quit()
 
