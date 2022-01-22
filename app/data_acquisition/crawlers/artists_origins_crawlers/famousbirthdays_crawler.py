a7cd4d175b72b79fd3393ae52957f4a435912234
Ben Bukai <bukai7ben@gmail.com>
2022-01-22 18:17:17 +0200

    Find more artists from famousbirthdays crawler

--- app/data_acquisition/crawlers/artists_origins_crawlers/famousbirthdays_crawler.py
+++ app/data_acquisition/crawlers/artists_origins_crawlers/famousbirthdays_crawler.py
@@ -1,6 +1,8 @@
 """
 This module finds artists origins from famous birthdays site
 """
+from pathlib import Path
+
 import pandas as pd
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
@@ -17,16 +19,19 @@ def get_artists_origins(df: pd.DataFrame) -> pd.DataFrame:
     artists = df["artist_name"].unique().tolist()
     driver = webdriver.Chrome("chromedriver.exe")
     driver.get("https://www.famousbirthdays.com/")
-    artists_df = pd.DataFrame(columns=["artist_name", "age", "origin"])
+    artists_df = pd.DataFrame(columns=["artist_name", "origin"])
+    count = 1
+    i = 0
 
     for artist in artists:
+        i += 1
         origin, age = "", ""
         input_element = driver.find_element(By.ID, "main-search")
         input_element.send_keys(artist)
         driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
 
         try:
-            age = driver.find_element(By.XPATH, "//a[contains(text(),'years old')]").text.split(" ")[0]
+            # age = driver.find_element(By.XPATH, "//a[contains(text(),'years old')]").text.split(" ")[0]
             origin = driver.find_element(By.XPATH, "//h6[contains(text(),'Birthplace')]/parent::div").text.split("\n")[
                 1]
         except NoSuchElementException:
@@ -35,8 +40,8 @@ def get_artists_origins(df: pd.DataFrame) -> pd.DataFrame:
                     1]
             except NoSuchElementException:
                 try:
-                    age = driver.find_element(By.XPATH, "//span[contains(text(),'age')]/parent::a").text.split("age ")[
-                        1]
+                    # age = driver.find_element(By.XPATH, "//span[contains(text(),'age')]/parent::a").text.split("age ")[
+                    #     1]
                     origin = \
                         driver.find_element(By.XPATH, "//h6[contains(text(),'Birthplace')]/parent::div").text.split(
                             "\n")[1]
@@ -44,8 +49,18 @@ def get_artists_origins(df: pd.DataFrame) -> pd.DataFrame:
                     continue
         finally:
             if origin:
-                found_df = pd.DataFrame([[artist, age, origin]], columns=["artist_name", "age", "origin"])
-                artists_df.append(found_df, ignore_index=True)
+                print(f"{count} : {artist} from {origin}")
+                count += 1
+                found_df = pd.DataFrame([[artist, origin]], columns=["artist_name", "origin"])
+                artists_df = pd.concat([artists_df, found_df], ignore_index=True)
 
     driver.quit()
     return artists_df
+
+
+# df = get_artists_origins(pd.read_csv(Path.cwd().parent.parent.parent / "data" / "artist_not_found.csv"))
+# df.to_csv(
+#     path_or_buf=Path.cwd().parent.parent.parent / "data" / "artists_and_origins_famousbirthdays2.csv",
+#     sep=",",
+#     columns=df.columns
+# )
--- app/data_acquisition/crawlers/artists_origins_crawlers/origins_crawlers_manager.py
+++ app/data_acquisition/crawlers/artists_origins_crawlers/origins_crawlers_manager.py
@@ -19,25 +19,31 @@ def artist_origin_manager(df: pd.DataFrame) -> pd.DataFrame:
     """
     df["origin"] = np.NaN
     df_famous = pd.read_csv(Path.cwd().parent.parent.parent / "data" / "artists_and_origins_famousbirthdays.csv")
+    df_famous = df_famous.rename(columns={'artist': 'artist_name'})
+    df_famous2 = pd.read_csv(Path.cwd().parent.parent.parent / "data" / "artists_and_origins_famousbirthdays2.csv")
     df_ranker = pd.read_csv(Path.cwd().parent.parent.parent / "data" / "artists_and_origins_ranker.csv")
     df_britannica = pd.read_csv(Path.cwd().parent.parent.parent / "data" / "artists_and_origins_britannica.csv")
     # todo: remove this
     found_counter = 1
     not_found_counter = 1
+    not_found_list = df["artist_name"].unique().tolist()
     for artist in df["artist_name"].unique().tolist():
+        if artist == "Ed Sheeran":
+            print("")
         # for crawler in [famousbirthdays_crawler.get_artists_origins,
         #                 ranker_crawler.get_artists_origins,
         #                 britannica_crawler.get_artists_origins]:
         # todo: remove this
-        for df_crawler in [df_famous, df_ranker, df_britannica]:
+        for df_crawler in [df_famous, df_ranker, df_britannica,df_famous2]:
             try:
                 # df_crawler = crawler()
                 row = df_crawler.index[df_crawler["artist_name"] == artist].tolist()[0]
                 artist_origin = df_crawler.loc[row, "origin"]
-                df.loc[df["artist_name"] == artist, "artist_origin"] = artist_origin
+                df.loc[df["artist_name"] == artist, "origin"] = artist_origin
                 # todo: remove this
                 found_counter += 1
                 print(f"found = {found_counter}")
+                not_found_list.remove(artist)
                 break
             except Exception:
                 not_found_counter += 1
@@ -50,6 +56,13 @@ def artist_origin_manager(df: pd.DataFrame) -> pd.DataFrame:
         path_or_buf=Path.cwd().parent.parent.parent / "data" / "04_origin_feature_update_from_crawler.csv",
         sep=",",
         columns=df.columns
+    )
+    dict = {'artist_name': not_found_list}
+    not_found_df = pd.DataFrame(dict)
+    not_found_df.to_csv(
+        path_or_buf=Path.cwd().parent.parent.parent / "data" / "artist_not_found.csv",
+        sep=",",
+        columns=not_found_df.columns
     )
     # todo: remove this
     print("saved_csv")
